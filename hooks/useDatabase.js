64c7a60ab4980ab4af8413993f9dc3c0a3f481eb
Frank Benjamin <benjaminarias3197@gmail.com>
2022-07-27 12:59:38 +0200

    porfi

--- hooks/useDatabase.js
+++ hooks/useDatabase.js
@@ -5,11 +5,11 @@ import useCollection from "./useCollection";
 // Custom hook for accessing Stylebjonas database | API => services
 const useDatabase = ( database ) => {
 
-      // const endPointDatabase = `/api/mongodb/database`
-      const endPointDatabase = `https://www.stylebjonas.com/api/mongodb/database`
+      const endPointDatabase = `/api/mongodb/database`
+      // const endPointDatabase = `https://www.stylebjonas.com/api/mongodb/database`
 
-      // const endPointCollection = `/api/mongodb/collection`
-      const endPointCollection = `https://www.stylebjonas.com/api/mongodb/collection`
+      const endPointCollection = `/api/mongodb/collection`
+      // const endPointCollection = `https://www.stylebjonas.com/api/mongodb/collection`
 
       const [collectionNames, setCollectionNames] = useState(null);
 
@@ -33,7 +33,8 @@ const useDatabase = ( database ) => {
                         setCollectionNames(response.data.data)
 
                         setIsLoaded(true)
-                        
+                        setIsErrorEncountered(false)
+
                         return response.data.data
                   })
                   .catch( error => {
